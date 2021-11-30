1a38735c3c30452e165094cf53df251f4bc319a4
Tarran-Sidhaarth <tarransidhaarth@gmail.com>
2021-11-30 18:21:49 +0530

    [Feature] Addition of Histogram

--- analyse_ebook.py
+++ analyse_ebook.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 from book_utils import analyse_ebook
-
+from book_utils import gethistogram #what tarran added
 print(sys.argv)
 assert len(sys.argv) == 2, "Usage: ./analyse_ebook.py filename"
 filename = sys.argv[1]
